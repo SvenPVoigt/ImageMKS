740d146e9592f4672cea23fce839ef3fa9a26f2d
Sven <svoigt6@gatech.edu>
2019-03-09 20:03:20 -0500

    main functionality

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='ImageMKS',
+    version='0.1.0dev',
+    packages=['imagemks',],
+    license='The MIT License (MIT)',
+    long_description=open('README.md').read(),
+    author='Sven Voigt',
+    author_email='svenpvoigt@gmail.com',
+    url='http://pypi.python.org/pypi/ImageMKS/',
+    description='Sharing segmentation frameworks.',
+    install_requires=[
+        "PyTorch >= 1.0.1",
+        "scikit-image >= 0.14.1",
+        "SciPy >= 1.1.0"
+    ],
+)
