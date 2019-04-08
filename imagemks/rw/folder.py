3361008fece38973dfbd2a19d0335795a223f902
Sven <svoigt6@gatech.edu>
2019-04-08 13:50:19 -0400

    updated the rw capabilities

--- imagemks/rw/folder.py
+++ imagemks/rw/folder.py
@@ -3,6 +3,8 @@ from PIL import Image
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
 from random import shuffle
+from scipy.io import loadmat, savemat
+import numpy as np
 
 
 class readlist(Dataset):
@@ -53,6 +55,64 @@ class readlist(Dataset):
 
     def __getitem__(self, idx):
         return self.T[self.mode](Image.open(self.path+self.files[idx]))
+
+
+class rwtype(Dataset):
+    def __init__(self, folderpath, mode='r', order=None, prefix=None, ftype=None):
+        super(rwtype, self).__init__()
+
+        assert mode in {'r', 'w'}, 'Mode needs to be r or w!'
+
+        assert any( (all( (mode=='w', prefix, ftype) ), mode=='r') ), 'prefix and ftype need to be defined for w mode.'
+
+        if prefix:
+            self.pre = prefix
+        else:
+            self.pre = ''
+
+        if ftype:
+            self.ftype = ftype
+        else:
+            self.ftype = '.png'
+
+        self.path = folderpath
+        self.order = order
+
+    def __len__(self):
+        self.files = listdir(folderpath)
+        self.files = list(i for i in self.files if path.isfile(path.join(folderpath, i)))
+        if ftype:
+            self.files = list(i for i in self.files if i[-len(ftype):]==ftype)
+        if prefix:
+            self.files = list(i for i in self.files if i[:len(prefix)]==prefix)
+        return len(self.files)
+
+    def __str__(self):
+        return 'Is %s a directory? '%self.path + str(path.isdir(self.path))
+
+    def print_all_files_(self):
+        return '\n' + ',    '.join(self.files)
+
+    def __getitem__(self, idx):
+        if self.order:
+            num = '{number:0{width}d}'.format(width=self.order, number=idx)
+        else:
+            num = str(idx)
+
+        return Image.open(self.path+self.pre+num+self.ftype)
+
+    def __setitem__(self, idx, val):
+        if self.order:
+            num = '{number:0{width}d}'.format(width=self.order, number=idx)
+        else:
+            num = str(idx)
+
+        if ftype in {'.jpeg', '.jpg', '.png', '.PNG', '.tif'}:
+            val.save(self.path+self.pre+num+self.ftype)
+        elif ftype == '.mat':
+            savemat(self.path+self.pre+num+self.ftype, val)
+        elif ftype == '.npy':
+            np.save(self.path+self.pre+num+self.ftype, val)
 
 
 
